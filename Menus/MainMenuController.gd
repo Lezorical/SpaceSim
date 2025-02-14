bd3859bd3441ddfc04b086e3488e5e85ba488db5
Lezorical <juan.arreola121599@gmail.com>
2025-02-14 14:55:57 -0800

    added menu and smoothing

--- Menus/MainMenuController.gd
+++ Menus/MainMenuController.gd
@@ -1,9 +1,12 @@
 extends Control
 
+var MainMenuCameraPosition
+var OptionsMenuCameraPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$CameraHolder/MainMenuCamera.make_current()
+	$CameraHolder/MainMenuCamera.position_smoothing_enabled
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -15,8 +18,12 @@ func _on_start_button_pressed() -> void:
 
 #option button Actions
 func _on_options_button_pressed() -> void:
-	pass # Replace with function body.
+	$CameraHolder.position.x += 1920
 
 #Quit button Actions
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+	
+#BackButton
+func _on_back_button_pressed() -> void:
+	$CameraHolder.position.x -= 1920
