6607bf99d140948213d645edf3beafce385b3bd4
Lezorical <juan.arreola121599@gmail.com>
2025-02-15 17:24:03 -0800

    added select pointer

---
+++ Scripts/ray_cast_3d.gd
@@ -0,0 +1,24 @@
+extends RayCast3D
+
+var collider = $".".get_collider()
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#$"../../InteractSprite".hide()
+	await get_tree().create_timer(0.01).timeout
+	#$"../../ISCOLLIDING".text = str("COLLIDING: ", is_colliding())
+	
+	if is_colliding():
+		var collider = get_collider()
+		if collider.is_in_group("interactable"):
+			$"../../ISCOLLIDING".text = str("COLLIDING: ", is_colliding())
+			$"../../InteractSprite".visible = true
+			
+	else:
+		$"../../ISCOLLIDING".text = str("COLLIDING: ", is_colliding())
+		$"../../InteractSprite".hide()
