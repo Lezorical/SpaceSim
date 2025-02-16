f7293e4832d79a64935c25d870c5cdd9d9b665f9
Lezorical <juan.arreola121599@gmail.com>
2025-02-16 14:19:26 -0800

    removed plugins temporaily

--- addons/hoimar.planetgen/scripts/terrain/ridged_noise_generator.gd
+++
@@ -1,8 +0,0 @@
-@tool
-class_name RidgedNoiseGenerator
-extends NoiseGenerator
-
-
-func evaluate(v: Vector3) -> float:
-	var elevation: float = 1.0 - abs(_simplex.get_noise_3dv(center + v))
-	return elevation * elevation * strength
