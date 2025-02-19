5624a88f8a859d671db89f6926dfb37018b01777
Lezorical <juan.arreola121599@gmail.com>
2025-02-18 16:28:00 -0800

    load manager

--- Scripts/pauseSaveButton.gd
+++ Scripts/pauseSaveButton.gd
@@ -3,25 +3,23 @@ extends Button
 
 var settings = ConfigFile.new()
 
-#Settings adjusters
-@onready var masterSound : float = $"../Sound/Master Sound".value
-@onready var musicSound : float= $"../Sound/Music Sound".value
-@onready var sfxSound : float = $"../Sound/SFX Sound".value
-@onready var looksensitivity : float = $"../Look Sensitivity".value
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
 func _on_pressed() -> void:
+	var masterSound : float = $"../Sound/Master Sound".value
+	var musicSound : float= $"../Sound/Music Sound".value
+	var sfxSound : float = $"../Sound/SFX Sound".value
+	var looksensitivity : float = $"../Look Sensitivity".value
+	
 	settings.set_value("master_volume", "volume", masterSound)
 	settings.set_value("music_volume", "volume", musicSound)
 	settings.set_value("sfx_volume", "volume", sfxSound)
 	settings.set_value("look_sensitivity", "sensitivity", looksensitivity)
 	
-	settings.save("res://settings.cfg")
+	settings.save("res://saves/settings.cfg")
 
 
 
