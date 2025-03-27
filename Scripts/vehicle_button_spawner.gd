8d70e5a00185985804f5f79ea8da040010e48c3b
Lezorical <juan.arreola121599@gmail.com>
2025-03-26 22:46:23 -0700

    updated

--- Scripts/vehicle_button_spawner.gd
+++ Scripts/vehicle_button_spawner.gd
@@ -10,10 +10,21 @@ func spawn_vehicle():
 	var block_placement = ConfigFile.new()
 	block_placement.load("res://saves/" + craft_name + ".cfg")
 	
-	var SpaceCraftParent : Area3D = Area3D.new()
+	#var SpaceCraftParent : Area3D = Area3D.new()
+	var SpaceCraftParent : Node3D = Node3D.new()
+	
 	SpaceCraftParent.name = craft_name
-	SpaceCraftParent.position = Vector3(-20,-1.5,0) #<-- Location above platform -20,8,-50 <--Middle location of Scaffold
+	
+#	SpaceCraftParent.gravity_scale = 0
+	#SpaceCraftParent.linear_velocity = Vector3(1,0,0)
+	#SpaceCraftParent.mass = 0.01
+	
+	#var SpaceCraftParentMesh : MeshInstance3D = MeshInstance3D.new()
+	#SpaceCraftParentMesh.create_convex_collision()
+	
+	SpaceCraftParent.position = Vector3(-20,-2.8,0) #<-- Location above platform -20,8,-50 <--Middle location of Scaffold
 	get_node("/root/Main").add_child(SpaceCraftParent)
+	#get_node("/root/Main/spaceship").add_child(SpaceCraftParentMesh)
 	
 	for section in block_placement.get_sections():
 		var scene_path = block_placement.get_value(section, "BlockType")
@@ -24,5 +35,5 @@ func spawn_vehicle():
 		
 		instance.global_position = position
 		SpaceCraftParent.add_child(instance)
-		instance.freeze = 0
+		instance.freeze = 1
 		print("Spawned ", instance.name, " at position ", instance.global_position)
